b9fd585b98c67785a0caf3815acad5f19edd73ea
Gary Clemente <gary.clemente07@gmail.com>
2025-11-04 09:30:21 -0500

    Initial Commit

---
+++ FindMyPark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FindMyPark
+//
+//  Created by Gary Clemente on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FindMyPark/FindMyParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyParkApp.swift
+//  FindMyPark
+//
+//  Created by Gary Clemente on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FindMyParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
